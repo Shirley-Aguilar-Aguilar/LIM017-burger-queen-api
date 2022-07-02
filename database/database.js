d9b2f86e5938683612e54f6e49fd2d51d6a6e4d0
Shirley Aguilar <shirley.aguilar.agui@gmail.com>
2022-07-01 19:45:23 -0500

    usando variables de entorno para despliegue

--- database/database.js
+++ database/database.js
@@ -1,9 +1,10 @@
+/* eslint-disable max-len */
 const Sequelize = require('sequelize');
 // database, username,password
 // la conexión permite crear tablas
 
-const postgreConnection = new Sequelize('apiburguerv1', 'postgres', '123456', {
-  host: 'localhost', // lugar donde esta la base de datos
+const postgreConnection = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_USER_PASSWORD, {
+  host: process.env.DB_HOSTNAME, // lugar donde esta la base de datos
   port: 5432,
   dialect: 'postgres',
   logging: false,
